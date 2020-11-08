d2d75ce57922c7ebcbb0d17d877406e42e16ac12
Amitoj Singh <amitojravi@gmail.com>
2020-11-08 12:33:28 +0800

    Ex089 for loop

--- ex089.py
+++ ex089.py
@@ -5,24 +5,23 @@ string_list = []
 
 
 #itering through the string
-for i in range(len(string)):
+for i,char in enumerate(string):
     #capitalizing first letter
     if i == 0:
-        string_list.append(string[i].upper())
+        string_list.append(char.upper())
     #checking if there is punctuation before the letter.
     elif string[i - 2] in '?.!' and i > 1:
-        string_list.append(string[i].upper())
+        string_list.append(char.upper())
     #if there is an "i" between spaces I want to capitalize it
-    elif string[i] == "i" and i != 0 and i != len(string) - 1 and string[i - 1] == ' ' and string[i + 1] == ' ':
-        string_list.append(string[i].upper())
+    elif char == "i" and i != len(string) - 1 and string[i - 1] == ' ' and string[i + 1] == ' ':
+        string_list.append(char.upper())
     #if there is an "i" with a space behind it, and punctuation next to it, I want to capitalize it.
-    elif string[i] == "i" and i != 0 and i != len(string) - 1 and string[i - 1] == ' ' and string[i + 1] in '?,.!':
-        string_list.append(string[i].upper())
+    elif char == "i" and i != len(string) - 1 and string[i - 1] == ' ' and string[i + 1] in '?,.!':
+        string_list.append(char.upper())
     #if there is nothing else, then i want to just add the letter
     else:
-        string_list.append(string[i])
+        string_list.append(char)
     
     
 #printing the list
-for x in string_list:
-    print(x, end='')
+print(''.join(string_list))
