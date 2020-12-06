9b1ba1cd1bf628a5bf38c9d97fda2a5ee3bfe59a
Amitoj Singh <amitojravi@gmail.com>
2020-12-06 11:11:38 +0800

    Added comments to algorithm

--- ex092b.py
+++ ex092b.py
@@ -9,15 +9,15 @@ from math import sqrt
 
 def prime_or_not(number):
     #loop that runs until half the number
-    if number < 2:
+    if number < 2: #if the number is smaller than 2 then it cannot be a prime num
         return False
-    if number == 2:
+    if number == 2: #if the number is 2 then it is a prime number
         return True
         
-    for i in range(2, round(sqrt(number)) + 1):
+    for i in range(2, round(sqrt(number)) + 1): #in the algorithm, we can just do it from 2 to the square root of the number you 
         if number % i == 0: #if it can (integer) divide without any remainders
             return False
-    return True
+    return True #otherwise if there are no numbers which are able to divide with the number, then it is prime.
 
 if __name__ == '__main__':
     num = int(input("Number: "))
