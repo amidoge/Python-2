e46894453ab94743e343a2deffe552c81b084598
amidoge <52399049+amidoge@users.noreply.github.com>
2020-12-20 11:23:58 +0800

    Changed condition statements and function name

--- ex097.py
+++ ex097.py
@@ -1,7 +1,7 @@
 #Making a good password
 #importing the functions from previous exercises
 from ex094 import random_password #importing my old password generator function
-from ex096b import password_checker #importing my old password checker function
+from ex096b import password_checker as valid_password #importing my old password checker function
 '''
 Using your solutions to Exercises 94 and 96, write a
 program that generates a random good password and displays
@@ -12,15 +12,12 @@ in the file that you create for this exercise.
 '''
 
 if __name__ == '__main__':
-    count = 0 #counting how many times it took to generate a new password which finally is verified
+    count = 1 #counting how many times it took to generate a new password which finally is verified
     password = random_password() #making the first random password
-    result = password_checker(password) #verifying the first random password
-    while result == False: #if it is false it is going to keep doing this until it is true
+    while not valid_password(password): #if it is false it is going to keep doing this until it is true
         count += 1 #keep counting until result == True
         password = random_password()
-        result = password_checker(password)
     #once result == True...
-    count += 1 #I need to add another because there needs to be the extra round for the good password as well
     if count == 1: #this is for grammar purposes only
         print(f'It took {count} try to generate a good password')
     else:    
